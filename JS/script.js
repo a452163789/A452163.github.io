5bcccdab468921a5d2ea7f85b12d4b97520945cd
A452163 <a452163789@outlook.com>
2024-08-25 12:15:59 +0800

    音乐更新

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,19 @@
 // 音频播放器1
-var audio1 = new Audio('⟪牢大の小曲⟫.mp3');
+var audio1 = new Audio('AUTOMOTIVO BAYSIDE.mp3');
 audio1.loop = true;
 var demo1 = document.querySelector('#demo1');
+
+// 音频播放器2
+var audio2 = new Audio('ONCE UPON A TIME.mp3');
+audio2.loop = true;
+
+// 点击 #container1 时的处理
 document.querySelector('#container1').addEventListener('click', function() {
   if (audio1.paused) {
+    // 如果audio1是暂停的，则播放它并停止audio2
+    if (!audio2.paused) {
+      audio2.pause();
+    }
     audio1.currentTime = 0;
     audio1.play().catch(error => console.error('Error playing audio1:', error));
   } else {
@@ -12,11 +22,13 @@ document.querySelector('#container1').addEventListener('click', function() {
   demo1.style.display = demo1.style.display === 'none' ? 'block' : 'none';
 });
 
-// 音频播放器2
-var audio2 = new Audio('ONCE UPON A TIME.mp3');
-audio2.loop = true;
+// 点击 #container2 时的处理
 document.querySelector('#container2').addEventListener('click', function() {
   if (audio2.paused) {
+    // 如果audio2是暂停的，则播放它并停止audio1
+    if (!audio1.paused) {
+      audio1.pause();
+    }
     audio2.currentTime = 0;
     audio2.play().catch(error => console.error('Error playing audio2:', error));
   } else {
