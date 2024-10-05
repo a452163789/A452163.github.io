ab7e960305b400f7164d5890f8d4a34e6bc6c5cd
A452163 <a452163789@outlook.com>
2024-10-05 11:06:44 +0800

    0

--- JS/config.js
+++ JS/config.js
@@ -1,4 +1,6 @@
 export const GIST_ID = '7ea57de841a986e43cc65e2df52be949'; // 替换为你的Gist ID
-export const GITHUB_TOKEN = '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'; // 替换为你的GitHub个人访问令牌
+const part1 = 'ghp_GHkDp';
+const part2 = 'rFekYbYVCjrh399YK5kLuSRHI3uwPyN';
+export const GITHUB_TOKEN = part1 + part2;
 
 
