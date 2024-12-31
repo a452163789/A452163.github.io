b44107eb2127236160d4d6280f13a4a0cda311a9
A452163 <168240179+A452163@users.noreply.github.com>
2024-12-31 23:09:02 +0800

    Update config.js

--- JS/config.js
+++ JS/config.js
@@ -1,6 +1,6 @@
 export const GIST_ID = '7ea57de841a986e43cc65e2df52be949'; // 替换为你的Gist ID
-const part1 = 'ghp_GHkDp';
-const part2 = 'rFekYbYVCjrh399YK5kLuSRHI3uwPyN';
+const part1 = 'ghp_c47KZ';
+const part2 = 'A7TM49unLBwlzT3wxbHnWXjgL3WMdCC';
 export const GITHUB_TOKEN = part1 + part2;
 
 
